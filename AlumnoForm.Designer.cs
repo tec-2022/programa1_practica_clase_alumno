b89fccd35388ebfc98fa62bffbe3208bbc4a8284
Fredy Luis vidalon lozano <kdl_milan@hotmail.com>
2023-09-26 23:52:55 -0700

    Agregar archivos de proyecto.

---
+++ AlumnoForm.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace programa1_practica_clase_alumno
+{
+    partial class AlumnoForm
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CapturarDesplegarButton = new System.Windows.Forms.Button();
+            this.LimpiarButton = new System.Windows.Forms.Button();
+            this.SalirButton = new System.Windows.Forms.Button();
+            this.ResultadoTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // CapturarDesplegarButton
+            // 
+            this.CapturarDesplegarButton.Location = new System.Drawing.Point(95, 53);
+            this.CapturarDesplegarButton.Name = "CapturarDesplegarButton";
+            this.CapturarDesplegarButton.Size = new System.Drawing.Size(93, 46);
+            this.CapturarDesplegarButton.TabIndex = 0;
+            this.CapturarDesplegarButton.Text = "CAPTURAR";
+            this.CapturarDesplegarButton.UseVisualStyleBackColor = true;
+            this.CapturarDesplegarButton.Click += new System.EventHandler(this.CapturarDesplegarButton_Click);
+            // 
+            // LimpiarButton
+            // 
+            this.LimpiarButton.Location = new System.Drawing.Point(95, 115);
+            this.LimpiarButton.Name = "LimpiarButton";
+            this.LimpiarButton.Size = new System.Drawing.Size(93, 46);
+            this.LimpiarButton.TabIndex = 1;
+            this.LimpiarButton.Text = "LIMPIAR";
+            this.LimpiarButton.UseVisualStyleBackColor = true;
+            this.LimpiarButton.Click += new System.EventHandler(this.LimpiarButton_Click);
+            // 
+            // SalirButton
+            // 
+            this.SalirButton.Location = new System.Drawing.Point(95, 173);
+            this.SalirButton.Name = "SalirButton";
+            this.SalirButton.Size = new System.Drawing.Size(93, 46);
+            this.SalirButton.TabIndex = 2;
+            this.SalirButton.Text = "SALIR";
+            this.SalirButton.UseVisualStyleBackColor = true;
+            this.SalirButton.Click += new System.EventHandler(this.SalirButton_Click);
+            // 
+            // ResultadoTextBox
+            // 
+            this.ResultadoTextBox.BackColor = System.Drawing.SystemColors.Window;
+            this.ResultadoTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ResultadoTextBox.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ResultadoTextBox.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.ResultadoTextBox.Location = new System.Drawing.Point(252, 47);
+            this.ResultadoTextBox.Multiline = true;
+            this.ResultadoTextBox.Name = "ResultadoTextBox";
+            this.ResultadoTextBox.Size = new System.Drawing.Size(295, 199);
+            this.ResultadoTextBox.TabIndex = 3;
+            // 
+            // AlumnoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(637, 328);
+            this.Controls.Add(this.ResultadoTextBox);
+            this.Controls.Add(this.SalirButton);
+            this.Controls.Add(this.LimpiarButton);
+            this.Controls.Add(this.CapturarDesplegarButton);
+            this.Name = "AlumnoForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CapturarDesplegarButton;
+        private System.Windows.Forms.Button LimpiarButton;
+        private System.Windows.Forms.Button SalirButton;
+        private System.Windows.Forms.TextBox ResultadoTextBox;
+    }
+}
+
---
+++ AlumnoForm.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace programa1_practica_clase_alumno
+{
+    public partial class AlumnoForm : Form
+    {
+        private Alumno alumno;
+
+        public AlumnoForm()
+        {
+            InitializeComponent();
+        }
+
+        private void CapturarDesplegarButton_Click(object sender, EventArgs e)
+        {
+            // Usar MessageBox para capturar datos del usuario
+            string nombre = Prompt("Ingrese el nombre del alumno:");
+            if (string.IsNullOrWhiteSpace(nombre))
+            {
+                MessageBox.Show("Nombre no válido", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            string apellidos = Prompt("Ingrese los apellidos del alumno:");
+            if (string.IsNullOrWhiteSpace(apellidos))
+            {
+                MessageBox.Show("Apellidos no válidos", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            int numeroControl;
+            string numeroControlStr = Prompt("Ingrese el número de control:");
+            if (!int.TryParse(numeroControlStr, out numeroControl))
+            {
+                MessageBox.Show("Número de control no válido", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            byte semestre;
+            string semestreStr = Prompt("Ingrese el semestre:");
+            if (!byte.TryParse(semestreStr, out semestre))
+            {
+                MessageBox.Show("Semestre no válido", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            string carrera = Prompt("Ingrese la carrera:");
+            if (string.IsNullOrWhiteSpace(carrera))
+            {
+                MessageBox.Show("Carrera no válida", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Crear objeto Alumno
+            alumno = new Alumno(nombre, apellidos, numeroControl, semestre, carrera);
+
+            // Mostrar datos en el TextBox
+            ResultadoTextBox.Clear(); // Limpia el TextBox antes de mostrar nuevos datos
+            ResultadoTextBox.AppendText("Datos del alumno" + Environment.NewLine);
+            ResultadoTextBox.AppendText("Nombre del alumno: " + alumno.Nombre + " " + alumno.Apellidos + Environment.NewLine);
+            ResultadoTextBox.AppendText("Numero de control: " + alumno.NumeroControl + Environment.NewLine);
+            ResultadoTextBox.AppendText("Semestre: " + alumno.Semestre + Environment.NewLine);
+            ResultadoTextBox.AppendText("Carrera: " + alumno.Carrera);
+        }
+
+        private string Prompt(string promptText)
+        {
+            // Muestra un MessageBox con un cuadro de texto para que el usuario ingrese datos
+            Form prompt = new Form()
+            {
+                Width = 500,
+                Height = 150,
+                FormBorderStyle = FormBorderStyle.FixedDialog,
+                Text = promptText,
+                StartPosition = FormStartPosition.CenterScreen
+            };
+            TextBox textBox = new TextBox() { Left = 50, Top = 20, Width = 400 };
+            Button okButton = new Button() { Text = "OK", Left = 350, Width = 100, Top = 70, DialogResult = DialogResult.OK };
+            okButton.Click += (sender, e) => { prompt.Close(); };
+            prompt.Controls.Add(textBox);
+            prompt.Controls.Add(okButton);
+            prompt.AcceptButton = okButton;
+
+            return prompt.ShowDialog() == DialogResult.OK ? textBox.Text : "";
+        }
+
+        private void LimpiarButton_Click(object sender, EventArgs e)
+        {
+            ResultadoTextBox.Clear(); // Limpia el TextBox
+
+        }
+
+        private void SalirButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit(); // Sale de la aplicación
+        }
+    }
+
+    // Clase Alumno (actualizada con apellidos)
+    class Alumno
+    {
+        public string Nombre { get; private set; }
+        public string Apellidos { get; private set; }
+        public int NumeroControl { get; private set; }
+        public byte Semestre { get; private set; }
+        public string Carrera { get; private set; }
+
+        public Alumno(string nombre, string apellidos, int numeroControl, byte semestre, string carrera)
+        {
+            Nombre = nombre;
+            Apellidos = apellidos;
+            NumeroControl = numeroControl;
+            Semestre = semestre;
+            Carrera = carrera;
+        }
+    }
+}
